39b150966d516713b7c7426f2c1f08dcc6111dee
VanTiennn <nguyenvantiennn0910@gmail.com>
2020-09-15 17:31:00 +0700

    test

--- src/routers/site.router.js
+++ src/routers/site.router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const siteController = require('../app/controllers/siteController');
 
 router.get('/', siteController.index);
-router.get('/search', siteController.search);
+router.get("/search", siteController.search);
 
 module.exports = router;
