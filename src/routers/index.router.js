eb55cafaf40c76433eb0cd1a164e5f2433581824
VanTiennn <nguyenvantiennn0910@gmail.com>
2020-09-15 17:17:27 +0700

    test

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,9 +1,11 @@
 const foodRouter = require('./food.router')
-const siteRouter = require('./site.router')
-const express = require('express')
+const siteRouter =   require('./site.router')
+const   express = require('express')
 const app = express()
 function router(app) {
-    app.use('/food', foodRouter)
+    app.use('/food',        foodRouter)
+
+    
     app.use('/site', siteRouter)
 }
 
